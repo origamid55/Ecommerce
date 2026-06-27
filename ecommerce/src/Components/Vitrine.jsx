0ba6b9621921d54228ecc3afc5208d295fb3c01b
origamid55 <reserva7408@gmail.com>
2026-06-27 19:24:39 -0300

    ok

--- ecommerce/src/Components/Vitrine.jsx
+++ ecommerce/src/Components/Vitrine.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import produtosEcommerce from '../Arquivos/ListaItens.js'
+import imagemPadrao from '../assets/sem-Foto.png'
 
 function Vitrine() {
   return (
@@ -8,13 +9,15 @@ function Vitrine() {
         { produtosEcommerce.map((e) =>(
         
             <div className='card' key={e.id}> 
+                <div className='img-container'>
                 <img src={e.image} onError={(event) => {
-    event.currentTarget.src = "https://placehold.co/600x400?text=Produto+Sem+Foto";
-  }}></img>
-                <h2>{e.title}</h2>
+    event.currentTarget.src = imagemPadrao }}></img>
+                </div>
+                <div className='titulo'><h2>{e.title}</h2></div>
                 <h3> R$ {e.price}</h3>
                 <br></br>
                 <h4>{e.description}</h4>
+                <div className='button'><button>Adicionar ao carrinho</button></div>
             </div>
         
         )) }
